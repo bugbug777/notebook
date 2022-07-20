5e847fe2f2244e7f11a9be71a5f1043bb148cf96
sihle <gubug777@gmail.com>
2022-07-20 19:51:35 +0800

    [add] 注入 default theme

--- src/.vuepress/config.js
+++ src/.vuepress/config.js
@@ -24,12 +24,6 @@ module.exports = {
     ['meta', { name: 'apple-mobile-web-app-capable', content: 'yes' }],
     ['meta', { name: 'apple-mobile-web-app-status-bar-style', content: 'black' }]
   ],
-
-  /**
-   * Theme configuration, here is the default theme configuration for VuePress.
-   *
-   * ref：https://v1.vuepress.vuejs.org/theme/default-theme-config.html
-   */
   themeConfig: {
     repo: '',
     editLinks: false,
@@ -56,19 +50,10 @@ module.exports = {
     ],
     sidebar: 'auto',
   },
-
-  /**
-   * Apply plugins，ref：https://v1.vuepress.vuejs.org/zh/plugin/
-   */
   plugins: {
     '@vuepress/plugin-back-to-top': true,
     'vuepress-plugin-smooth-scroll': true,
-    '@vuepress/plugin-medium-zoom': false,
   },
-
-  /**
-   * Use alias
-   */
   configureWebpack: {
     resolve: {
       alias: {
